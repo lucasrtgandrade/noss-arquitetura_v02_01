7ecf2abf0793638583a1a2ad72c87c59dd12ea10
Lucas Reategui <lucasrtgandrade@gmail.com>
2025-06-10 17:43:56 -0300

    Servidor Teste

---
+++ public_html/views/contato.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <title>Contato</title>
+</head>
+
+<main>
+    <h2>Bem vindo</h2>
+    <nav>
+        <a href="?rota=home">Home</a>
+        <a href="?rota=sobre">Sobre</a>
+        <a href="?rota=contato">Contato</a>
+    </nav>
+</main>
+</html>
---
+++ public_html/views/sobre.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <title>Sobre</title>
+</head>
+
+<main>
+    <h2>Página Sobre</h2>
+    <nav>
+        <a href="/index">Home</a>
+        <a href="/sobre">Sobre</a>
+        <a href="/contato">Contato</a>
+    </nav>
+</main>
+</html>
